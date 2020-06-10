dd976fcd22f16e0d3b0a1846a42ae8f5a839a028
Diego Ramos <diego.coderamos@gmail.com>
2020-06-10 00:32:48 -0300

    update api URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://172.28.43.173:3333",
+  baseURL: "http://172.28.43.173:3333", //windows
+  // baseURL: "http://192.168.0.104:3333", //ubuntu
 });
 
 export default api;
